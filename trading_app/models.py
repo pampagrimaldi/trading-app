2ed4a9e4871b9169c4f375a7c8ae13b480cde835
pampagrimaldi <pampagrimaldi@gmail.com>
2023-12-04 18:19:16 +1100

    working files up to charting

--- trading_app/models.py
+++ trading_app/models.py
@@ -5,10 +5,9 @@ from sqlalchemy import (
     String,
     DateTime,
     Numeric,
-    BigInteger,
     ForeignKey,
-    UniqueConstraint,
 )
+
 from sqlalchemy.orm import relationship
 
 
@@ -28,13 +27,12 @@ class StockPrice(Base):
     __tablename__ = 'stock_price'
     stock_id = Column(Integer, ForeignKey('stock.id'), primary_key=True)
     dt = Column(DateTime, primary_key=True, nullable=False)
-    close = Column(Numeric(10, 4), nullable=False)   # Adjusted precision and scale
-    high = Column(Numeric(10, 4), nullable=False)    # Adjusted precision and scale
-    low = Column(Numeric(10, 4), nullable=False)     # Adjusted precision and scale
-    trade_count = Column(Integer, nullable=False)    # New column for trade count
-    open = Column(Numeric(10, 4), nullable=False)    # Adjusted precision and scale
+    close = Column(Numeric(10, 4), nullable=False)
+    high = Column(Numeric(10, 4), nullable=False)
+    low = Column(Numeric(10, 4), nullable=False)
+    trade_count = Column(Integer, nullable=False)
+    open = Column(Numeric(10, 4), nullable=False)
     volume = Column(Numeric, nullable=False)
-    vwap = Column(Numeric(10, 4), nullable=False)    # New column for VWAP
+    vwap = Column(Numeric(10, 4), nullable=False)
 
-    # Define the relationship (optional, if you need to access Stock from StockPrice)
     stock = relationship("Stock")
--- trading_app/populate_db.py
+++ trading_app/populate_db.py
@@ -1,6 +1,6 @@
 import alpaca_trade_api as tradeapi
 from trading_app import models
-from trading_app.database import get_db, SessionLocal
+from trading_app.database import SessionLocal
 from trading_app.trader import api
 from sqlalchemy.orm import Session
 import logging
@@ -9,9 +9,7 @@ from tqdm import tqdm
 
 # Setup logging
 script_dir = os.path.dirname(os.path.realpath(__file__))
-project_root_dir = os.path.dirname(
-    script_dir
-)  # Move one directory up to the project root
+project_root_dir = os.path.dirname(script_dir)  # Move one directory up to the project root
 logs_dir = os.path.join(project_root_dir, "logs")
 
 # Create logs directory if it doesn't exist
@@ -24,43 +22,35 @@ logger.setLevel(logging.INFO)
 
 file_handler = logging.FileHandler(log_file_path)
 file_handler.setLevel(logging.INFO)
-file_handler.setFormatter(
-    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-)
+file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
 logger.addHandler(file_handler)
 
 
 # Function to insert stock data
-def insert_stock_data(session: Session, symbol: str, company: str, exchange: str, new_stock_added):
-    existing_stock = (session
-                      .query(models.Stock)
-                      .filter(models.Stock.symbol == symbol)
-                      .first())
-
+def insert_stock_data(session: Session, symbol: str, company: str, exchange: str):
+    existing_stock = session.query(models.Stock).filter(models.Stock.symbol == symbol).first()
     if not existing_stock:
-        new_stock = models.Stock(symbol=symbol, company=company,exchange=exchange)
+        new_stock = models.Stock(symbol=symbol, company=company, exchange=exchange)
         session.add(new_stock)
-        logger.info(f"Added new stock: {symbol}, {company}")
-        new_stock_added.append(True)
+        return True
+    return False
 
 
 try:
-    # Fetch assets and limit to the first 2000
-    all_assets = api.list_assets(asset_class="us_equity",
-                                 status="active")
-
+    # Fetch assets and limit to the first 1000
+    all_assets = api.list_assets(asset_class="us_equity", status="active")
     filtered_assets = [asset for asset in all_assets if asset.tradable][:1000]
 
-    new_stock_added = []  # List to track if new stocks are added
+    new_stock_count = 0  # Initialize a counter for new stocks
     with SessionLocal() as session:
         for asset in tqdm(filtered_assets):
-            insert_stock_data(session, asset.symbol, asset.name,asset.exchange, new_stock_added)
+            if insert_stock_data(session, asset.symbol, asset.name, asset.exchange):
+                new_stock_count += 1  # Increment counter for each new stock added
 
         session.commit()
-        logger.info("Database update completed successfully.")
-
-    if not new_stock_added:
-        logger.info("No new stocks were found to add.")
+        logger.info(f"{new_stock_count} new stocks were added to the database.")
+        if new_stock_count == 0:
+            logger.info("No new stocks were found to add.")
 
 except Exception as e:
     logger.error(f"Error during script execution: {e}")
