fc5f522708fa6f74c07dd025a1cec51a83531884
pampagrimaldi <pampagrimaldi@gmail.com>
2023-12-06 21:10:12 +1100

    developing opening_range_breakout algo

--- trading_app/strategies/opening_range_breakout.py
+++ trading_app/strategies/opening_range_breakout.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from trading_app import models
 from trading_app.database import SessionLocal
 from trading_app.trader import api
@@ -27,12 +27,20 @@ def opening_range_breakout(session: Session, strategy: str):
 			  .all())
 
 	symbols = [stock.symbol for stock in stocks]
-	# fetch data with alpaca api
-	# todo: it's delta -1 as its based on US (Change when using Aussie stocks)
-	start_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
-	bars = api.get_bars(symbols, TimeFrame.Minute, start_date, adjustment="raw").df
-	print('=== bars head ===')
-	print(bars.head())
+	current_date = (date.today() - timedelta(days=1)).isoformat()
+	print(f'current date {current_date}')
+	start_minute_bar = f'{current_date}T13:30:00Z'  # For daylight saving time
+	end_minute_bar = f'{current_date}T13:45:00Z'
+	bars = api.get_bars(symbols, TimeFrame.Minute, current_date, adjustment="raw").df
+
+	# masks
+	opening_range_mask = (bars.index >= start_minute_bar) & (bars.index < end_minute_bar)
+	# print first 10 items of symbol GDDY sort ascending on index
+	print('\t'*4 + '======== first 20 items GDDY ========')
+	print(bars[bars.symbol == 'GDDY'].head(20))
+	# print open range mask
+	print('\t'*4 + '======== opening range mask ========')
+	print(bars[opening_range_mask])
 
 
 # Example usage
